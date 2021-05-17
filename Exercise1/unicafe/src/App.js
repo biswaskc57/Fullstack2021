59e7770d8773a4a800eafb3331665ad1fe198e19
biswaskc57 <biswas.k.c@myy.haaga-helia.fi>
2021-05-17 11:00:46 +0300

    unicafe completed

--- Exercise1/unicafe/src/App.js
+++ Exercise1/unicafe/src/App.js
@@ -1,38 +1,49 @@
-import logo from "./logo.svg";
-import "./App.css";
 import React, { useState } from "react";
+
+const Statistics = (props) => {
+  return (
+    <div>
+      <table style={{ width: "40%" }}>
+        <tr>
+          <td style={{ width: "30%" }}>{props.text}:</td>
+          <td style={{ width: "70%" }}> {props.value}</td>
+        </tr>
+      </table>
+    </div>
+  );
+};
 
 const Statistic = (props) => {
   console.log(props.good);
   if (props.sum < 1) {
     return (
       <div>
-        <p>
-          <button onClick={props.goodHandler}>good</button>
-          <button onClick={props.badHandler}>bad</button>
-          <button onClick={props.neutralHandler}>neutral</button>
-        </p>
-        No feedbacks yet!
+        <button onClick={props.goodHandler}>good</button>
+        <button onClick={props.badHandler}>bad</button>
+        <button onClick={props.neutralHandler}>neutral</button>
+        <p>No feedbacks yet!</p>
       </div>
     );
   } else
     return (
       <div>
-        <h1>
-          <p>
-            <button onClick={props.goodHandler}>good</button>
-            <button onClick={props.badHandler}>bad</button>
-            <button onClick={props.neutralHandler}>neutral</button>
-          </p>
+        <button onClick={props.goodHandler}>good</button>
+        <button onClick={props.badHandler}>bad</button>
+        <button onClick={props.neutralHandler}>neutral</button>
 
-          <p>Statistics:</p>
-        </h1>
-        <h5>good: {props.good}</h5>
-        <h5>neutral: {props.neutral}</h5>
-        <h5>bad: {props.bad}</h5>
-        <h5>all: {props.sum}</h5>
-        <h5>average: {(props.sum / 3).toFixed(2)}</h5>
-        <h5>average: {(props.good / props.sum).toFixed(2) * 100}%</h5>
+        <h1>Statistics:</h1>
+        <div>
+          {" "}
+          <Statistics text="good" value={props.good} />
+          <Statistics text="neutral" value={props.neutral} />
+          <Statistics text="bad" value={props.bad} />
+          <Statistics text="all" value={props.sum} />
+          <Statistics text="average" value={(props.sum / 3).toFixed(2)} />
+          <Statistics
+            text="positive rate"
+            value={(props.good / props.sum).toFixed(2) * 100 + " %"}
+          />
+        </div>
       </div>
     );
 };
@@ -52,27 +63,20 @@ function App(props) {
     setNeutral(neutral + 1);
   };
 
-  const all = () => {
-    return good + bad + neutral;
-  };
   let sum = good + bad + neutral;
 
   return (
     <div className="App">
-      <h1>
-        Give feedback:
-        <p>
-          <Statistic
-            goodHandler={clickHandlerGood}
-            badHandler={clickHandlerBad}
-            neutralHandler={clickHandlerNeutral}
-            sum={sum}
-            good={good}
-            bad={bad}
-            neutral={neutral}
-          />
-        </p>
-      </h1>
+      <h1>Give feedback:</h1>
+      <Statistic
+        goodHandler={clickHandlerGood}
+        badHandler={clickHandlerBad}
+        neutralHandler={clickHandlerNeutral}
+        sum={sum}
+        good={good}
+        bad={bad}
+        neutral={neutral}
+      />
     </div>
   );
 }

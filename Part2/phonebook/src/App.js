46f833754b704c6c4c7fc8269953a0526b9459ee
biswaskc57 <biswas.k.c@myy.haaga-helia.fi>
2021-05-19 20:20:43 +0300

    exercise2.8 done

--- Part2/phonebook/src/App.js
+++ Part2/phonebook/src/App.js
@@ -3,17 +3,22 @@ import "./App.css";
 import React, { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newPhoneNumber, setNewPhoneNumber] = useState("");
+  const [filter, setFilter] = useState("");
 
-  const handleButtonChange = (event) => {
-    setNewName(event.target.value);
-  };
-
+  console.log(newPhoneNumber);
   const addPerson = (event) => {
     event.preventDefault();
     const personObject = {
       name: newName,
+      phone: newPhoneNumber,
     };
     const checkName = persons.map((allname, index) => {
       console.log(persons);
@@ -28,10 +33,32 @@ const App = () => {
 
   return (
     <div>
-      <h2>Phonebook</h2>
+      <p>
+        {" "}
+        Filter:{" "}
+        <input
+          name="phoneNumber"
+          onChange={(event) => setFilter(event.target.value)}
+          value={filter}
+        />
+      </p>
+      <h2>Add a new</h2>
       <form>
         <div>
-          name: <input onChange={handleButtonChange} />
+          name:{" "}
+          <input
+            name="name"
+            onChange={(event) => setNewName(event.target.value)}
+            value={newName}
+          />
+          <p>
+            Phone no:{" "}
+            <input
+              name="phoneNumber"
+              onChange={(event) => setNewPhoneNumber(event.target.value)}
+              value={newPhoneNumber}
+            />
+          </p>
         </div>
         <div>
           <button type="submit" onClick={addPerson}>
@@ -42,7 +69,9 @@ const App = () => {
       <h2>Numbers</h2>
       <div> {persons.name}</div>
       {persons.map((person) => (
-        <li key={person.name}>{person.name}</li>
+        <li key={person.name}>
+          {person.name}: {person.number}
+        </li>
       ))}
     </div>
   );

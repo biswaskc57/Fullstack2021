2f2bc09c8e053b1f11c3af164b8971ae6926a124
biswaskc57 <biswas.k.c@myy.haaga-helia.fi>
2021-05-26 13:40:46 +0300

    part 2 finished

--- Part2/phonebook/src/App.js
+++ Part2/phonebook/src/App.js
@@ -1,8 +1,11 @@
 import "./App.css";
+import "./index.css";
 import React, { useState, useEffect } from "react";
 import Filter from "./Components/Filter";
 import PersonForm from "./Components/PersonForm";
 import Persons from "./Components/Persons";
+import Notification from "./Components/Notification";
+
 import axios from "axios";
 import useService from "./Components/services/persons";
 
@@ -21,6 +24,8 @@ const App = (props) => {
   const [newName, setNewName] = useState("");
   const [newPhoneNumber, setNewPhoneNumber] = useState("");
   const [filter, setFilter] = useState("");
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [addMessage, setAddMessage] = useState(null);
 
   const nameHandler = (event) => setNewName(event.target.value);
 
@@ -33,7 +38,7 @@ const App = (props) => {
 
   const deletePersonOf = (id, name) => {
     let confirms = window.confirm("Press ok to delete  " + name);
-    confirms == true
+    confirms === true
       ? useService.deletePersons(id).then((response) => {
           console.log(response);
           setPersons(persons.filter((person) => person.id !== id));
@@ -56,6 +61,12 @@ const App = (props) => {
     if (!checkName.includes(newName) && newName !== "") {
       useService.create(personObject).then((response) => {
         setPersons(persons.concat(response));
+        setAddMessage(`Added ${newName}`);
+        setTimeout(() => {
+          setAddMessage(null);
+          console.log(setAddMessage);
+        }, 5000);
+
         console.log(response);
       });
     } else if (checkName.includes(newName)) {
@@ -63,34 +74,49 @@ const App = (props) => {
         "Do you want to change the phone number of " + newName + " ?"
       );
 
-      //
       if (confirms === true) {
         const newPerson = persons.filter(
           (person) => person.name === newName
-        )[0]; //returns a array of object which is same
+        )[0];
 
-        //changed person with the changed number
+        //duplicate object with same id created with the new phone number
         const changedPerson = { ...newPerson, number: newPhoneNumber };
 
+        //id of the object to be update
         const toUpdateId = newPerson.id;
 
-        useService.update(toUpdateId, changedPerson).then((response) => {
-          console.log(response);
-          setPersons(
-            persons.map((person) =>
-              person.id !== toUpdateId ? person : response
-            )
-          );
-          console.log(persons);
-        });
+        useService
+          .update(toUpdateId, changedPerson)
+          .then((response) => {
+            console.log(response);
+            setPersons(
+              persons.map((person) =>
+                person.id !== toUpdateId ? person : response
+              )
+            );
+            console.log(persons);
+          })
+          .catch((error) => {
+            setErrorMessage(
+              `Information of '${changedPerson.name}' has already been removed from server`
+            );
+            setTimeout(() => {
+              setErrorMessage(null);
+              console.log(errorMessage);
+            }, 5000);
+            setPersons(persons.filter((n) => n.id !== toUpdateId));
+          });
       } else alert("something went wrong");
     }
   };
   console.log(persons);
+  console.log(errorMessage);
   return (
-    <div>
+    <div style={{ margin: "auto" }}>
+      <h1>Phonebook:</h1>
+      <Notification message={errorMessage} addMessage={addMessage} />
       <Filter handler={filterHandler} />
-      <h2>Add a new:</h2>
+      <h1>Add a new:</h1>
       <PersonForm
         persons={persons}
         nameHandler={nameHandler}
@@ -98,7 +124,7 @@ const App = (props) => {
         addPerson={addPerson}
       />
       <h2>Numbers</h2>
-      <ul>
+      <ul className="persons">
         {persons
           .filter((name) => name.name.toLowerCase().includes(filter))
           .map((open, i) => (

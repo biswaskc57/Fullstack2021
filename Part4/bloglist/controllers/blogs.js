c73b89d9dcebb4cd944f2cd120509742daf82adb
biswaskc57 <biswas.k.c@myy.haaga-helia.fi>
2021-06-23 22:01:28 +0300

    bugs in 4.22 fixed

--- Part4/bloglist/controllers/blogs.js
+++ Part4/bloglist/controllers/blogs.js
@@ -12,51 +12,53 @@ blogsRouter.get("/", async (request, response) => {
 blogsRouter.get("/:id", async (request, response) => {
   const blog = await Blog.findById(request.params.id);
   if (blog) {
-    response.json(blog);
+    response.json(blog.toJSON());
   } else {
     response.status(404).end();
   }
 });
 
-blogsRouter.post("/", async (request, response) => {
+blogsRouter.post("/", async (request, response, next) => {
   const body = request.body;
-
   const decodedToken = jwt.verify(request.token, process.env.SECRET);
-  if (!request.token || !decodedToken.id) {
-    return response.status(401).json({ error: "token missing or invalid" });
+
+  try {
+    let bloglikes;
+    if (body.likes === undefined) {
+      bloglikes = 0;
+    } else {
+      bloglikes = body.likes;
+    }
+
+    if (!request.token || !decodedToken.id) {
+      return response.status(401).json({ error: "token missing or invalid" });
+    } else if (body.title === undefined || body.url === undefined) {
+      return response.status(400).json({ error: "title or url missing" });
+    } else {
+      const user = request.user;
+      //new Blog object is created.
+      const blog = new Blog({
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: bloglikes,
+        user: user._id,
+      });
+      console.log(blog);
+
+      //.save()method
+      // Inserts a new document with request parameters
+      const savedBlog = await blog.save();
+
+      user.blogs = user.blogs.concat(savedBlog._id);
+      await user.save();
+      console.log(savedBlog);
+
+      response.json(savedBlog.toJSON());
+    }
+  } catch (error) {
+    next(error);
   }
-  const user = await User.findById(decodedToken.id);
-  if (body.title === undefined || body.url === undefined) {
-    return response.status(400).json({ error: "title or url missing" });
-  }
-
-  let bloglikes;
-
-  if (body.likes === undefined) {
-    bloglikes = 0;
-  } else {
-    bloglikes = body.likes;
-  }
-
-  //new Blog object is created.
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: bloglikes,
-    user: user._id,
-  });
-  console.log(blog);
-
-  //.save()method
-  // Inserts a new document with request parameters
-  const savedBlog = await blog.save();
-
-  user.blogs = user.blogs.concat(savedBlog._id);
-  await user.save();
-  console.log(savedBlog);
-
-  response.json(savedBlog);
 });
 
 blogsRouter.delete("/:id", async (request, response, next) => {
@@ -80,41 +82,21 @@ blogsRouter.delete("/:id", async (request, response, next) => {
   }
 });
 
-blogsRouter.put("/:id", async (request, response) => {
-  const body = new Blog(request.body);
+blogsRouter.put("/:id", async (request, response, next) => {
+  const body = request.body;
 
-  if (!body.likes) {
-    body.likes = 0;
-  }
-  console.log(request.user._id.toString());
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes || 0,
+  };
 
-  const blogToUpdate = await Blog.findById(request.params.id);
-  console.log(blogToUpdate);
   try {
-    if (!blogToUpdate.user || !request.user._id) {
-      return response.status(404);
-    } else if (blogToUpdate.user.toString() === request.user._id.toString()) {
-      const blog = {
-        title: body.title,
-        author: body.author,
-        url: body.url,
-        likes: body.likes,
-      };
-      const updatedBlog = await Blog.findByIdAndUpdate(
-        request.params.id,
-        blog,
-        {
-          new: true,
-        }
-      );
-      return response.json(updatedBlog);
-    } else {
-      return response
-        .status(401)
-        .json({ error: "token is missing or invalid" });
-    }
-  } catch {
-    next(error);
+    await Blog.findByIdAndUpdate(request.params.id, blog, { new: true });
+    response.status(200).json(blog);
+  } catch (err) {
+    next(err);
   }
 });
 
--- Part4/bloglist/utils/middleware.js
+++ Part4/bloglist/utils/middleware.js
@@ -35,14 +35,12 @@ const tokenExtractor = (request, response, next) => {
 };
 
 const userExtractor = async (request, response, next) => {
-  if (!request.token) {
-    return response.status(401).json({ error: "token missing " });
+  if (request.token) {
+    const token = request.token;
+    const decodedToken = jwt.verify(token, process.env.SECRET);
+    const user = await User.findById(decodedToken.id);
+    request.user = user;
   }
-  const token = request.token;
-  const decodedToken = jwt.verify(token, process.env.SECRET);
-  const user = await User.findById(decodedToken.id);
-  request.user = user;
-
   next();
 };
 

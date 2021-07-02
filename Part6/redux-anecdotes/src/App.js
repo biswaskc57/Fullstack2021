ef88a5a194a3489c10f2989d3eae65aabb02ad69
biswaskc57 <biswas.k.c@myy.haaga-helia.fi>
2021-07-02 15:07:52 +0300

    part 6-5 -6.8 done

--- Part6/redux-anecdotes/src/App.js
+++ Part6/redux-anecdotes/src/App.js
@@ -1,13 +1,8 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { voteAnecdote } from "./reducers/anecdoteReducer";
 
 import AnecdoteList from "./components/AnecdoteList";
 import AnecdoteForm from "./components/AnecdoteForm";
 const App = () => {
-  const anecdotes = useSelector((state) => state);
-  const dispatch = useDispatch();
-
   return (
     <div>
       <h2>Anecdotes</h2>

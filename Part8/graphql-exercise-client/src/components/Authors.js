fb8b879a44dfccb285126180f3e32048575fff6d
biswaskc57 <biswas.k.c@myy.haaga-helia.fi>
2021-10-20 18:34:18 +0300

    Part 8.8 done

---
+++ Part8/graphql-exercise-client/src/components/Authors.js
@@ -0,0 +1,41 @@
+  
+import React from 'react'
+
+const Authors = ({authors}) => {
+
+  console.log("author is", authors)
+  if (!authors) {
+    return <div></div>
+  }
+  
+  
+
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>
+              born
+            </th>
+            <th>
+              books
+            </th>
+          </tr>
+          {authors.map(a =>
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+
+    </div>
+  )
+}
+
+export default Authors
